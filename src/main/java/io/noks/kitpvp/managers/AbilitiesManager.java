d8b54219a1ca93ee5f979a97ebf517f71351fdd2
Noksio <noksiopro@gmail.com>
2024-07-18 00:15:37 -0400

    Changed AbilitiesList to ImmutableList

--- src/main/java/io/noks/kitpvp/managers/AbilitiesManager.java
+++ src/main/java/io/noks/kitpvp/managers/AbilitiesManager.java
@@ -1,13 +1,12 @@
 package io.noks.kitpvp.managers;
 
-import java.util.HashSet;
 import java.util.Set;
 
 import com.avaje.ebean.validation.NotNull;
+import com.google.common.collect.ImmutableSet;
 
 import io.noks.kitpvp.Main;
 import io.noks.kitpvp.abstracts.Abilities;
-import io.noks.kitpvp.listeners.abilities.Sniper;
 import io.noks.kitpvp.listeners.abilities.Batman;
 import io.noks.kitpvp.listeners.abilities.Blink;
 import io.noks.kitpvp.listeners.abilities.Boxer;
@@ -24,6 +23,7 @@ import io.noks.kitpvp.listeners.abilities.Phantom;
 import io.noks.kitpvp.listeners.abilities.PvP;
 import io.noks.kitpvp.listeners.abilities.Reaper;
 import io.noks.kitpvp.listeners.abilities.Snail;
+import io.noks.kitpvp.listeners.abilities.Sniper;
 import io.noks.kitpvp.listeners.abilities.Spider;
 import io.noks.kitpvp.listeners.abilities.Stomper;
 import io.noks.kitpvp.listeners.abilities.Switcher;
@@ -32,34 +32,38 @@ import io.noks.kitpvp.listeners.abilities.Viper;
 import io.noks.kitpvp.listeners.abilities.Zeus;
 
 public class AbilitiesManager {
-	private final @NotNull Set<Abilities> abilities = new HashSet<Abilities>();
+	private final @NotNull Set<Abilities> abilities;
 	
 	public AbilitiesManager(Main main) {
-		abilities.add(new PvP());
-		abilities.add(new Sniper(main));
-		abilities.add(new Blink()); // Instancied
-		abilities.add(new Boxer());
-		abilities.add(new Fireman(main));
-		abilities.add(new Fisherman(main));
-		//abilities.add(new Gladiator(main)); // Instancied TODO: need to be TESTED
-		abilities.add(new Hulk(main));
-		abilities.add(new Kangaroo(main));
-		//abilities.add(new Ninja()); // Instancied
-		abilities.add(new Phantom(main));
-		abilities.add(new Reaper(main));
-		abilities.add(new Snail(main));
-		abilities.add(new Stomper(main));
-		abilities.add(new Switcher(main));
-		abilities.add(new Turtle(main));
-		abilities.add(new Viper(main));
-		abilities.add(new Zeus(main));
-		abilities.add(new CookieMonster(main));
-		abilities.add(new Magma(main));
-		abilities.add(new Monk(main));
-		abilities.add(new Batman(main));
-		abilities.add(new Spider(main));
-		abilities.add(new Chemist());
-		abilities.add(new Pacifist(main));
+		ImmutableSet.Builder<Abilities> abilitiesBuilder = ImmutableSet.builder();
+		
+		abilitiesBuilder.add(new PvP());
+		abilitiesBuilder.add(new Sniper(main));
+		abilitiesBuilder.add(new Blink()); // Instancied
+		abilitiesBuilder.add(new Boxer());
+		abilitiesBuilder.add(new Fireman(main));
+		abilitiesBuilder.add(new Fisherman(main));
+		//abilitiesBuilder.add(new Gladiator(main)); // Instancied TODO: need to be TESTED
+		abilitiesBuilder.add(new Hulk(main));
+		abilitiesBuilder.add(new Kangaroo(main));
+		//abilitiesBuilder.add(new Ninja()); // Instancied
+		abilitiesBuilder.add(new Phantom(main));
+		abilitiesBuilder.add(new Reaper(main));
+		abilitiesBuilder.add(new Snail(main));
+		abilitiesBuilder.add(new Stomper(main));
+		abilitiesBuilder.add(new Switcher(main));
+		abilitiesBuilder.add(new Turtle(main));
+		abilitiesBuilder.add(new Viper(main));
+		abilitiesBuilder.add(new Zeus(main));
+		abilitiesBuilder.add(new CookieMonster(main));
+		abilitiesBuilder.add(new Magma(main));
+		abilitiesBuilder.add(new Monk(main));
+		abilitiesBuilder.add(new Batman(main));
+		abilitiesBuilder.add(new Spider(main));
+		abilitiesBuilder.add(new Chemist());
+		abilitiesBuilder.add(new Pacifist(main));
+		
+		this.abilities = abilitiesBuilder.build();
 	}
 	
 	public Abilities getAbilityFromName(String name) {
